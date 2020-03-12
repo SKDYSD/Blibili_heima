03f5b8fbadaf78c0ac24651f40816ff3be442564
SKDYSD <18150116196m0@sina.cn>
2020-03-12 17:05:36 +0800

    1

---
+++ heima_课程合集.py
@@ -0,0 +1,92 @@
+# conding = utf-8
+
+
+#个人信息
+
+#"""
+#姓名：小明
+#年龄：18
+#性别：是男生
+#身高：1.75米
+#体重：75.0公斤
+#"""
+#name = "小明"
+#age = 18
+#gender = False
+#height = 1.75
+#weighe = 75.0
+#print(type(name))
+
+
+#买苹果增强版
+
+#输入苹果的价格
+#price = float(input("请输入苹果价格："))
+#输入苹果的重量
+#weight = float(input("请输入苹果的重量："))
+#计算支付总金额
+#price = float(price_str )
+#weight = float( weight_str)
+#money = price * weight
+#print("价格为：",money)
+
+
+#格式化输出
+#name = "大明"
+#student_no = 1
+#price = 8.5
+#weight = 7.5
+#money = price * weight
+#scale = 10.00
+#print("我的名字叫%s，请多多关照！" % name)
+#print("我的学号是%06d" % student_no)
+#print("苹果单价%.02f/斤，购买了%.02f斤，需要支付%.04f元" % (price,weight,money))
+#print("数据比例是%.02f%%" % scale)
+
+
+#判断语句（分支语句）
+#age = int(input("请输入年龄"))
+#if age >= 18:
+#    print("可以进网吧")
+#else:
+#    print("回家写作业")
+
+
+#逻辑运算
+"""
+age >= 0 and age <=120
+age >= 0 or age <=120
+"""
+#1、and  与
+#age = 26
+#if age >= 0 and age <=120:
+#    print("年龄正确")
+#else :
+#    print("年龄不正确")
+#2、or  或
+#python_score = 59
+#c_score = 60
+#if python_score >= 60 or c_score >= 60:
+#    print("考试通过")
+#else :
+#    print("考试不通过")
+#3、not  非
+#is_employee = False
+#if not is_employee:
+#    print("非本公司员工请勿入内")
+
+
+#elfi应用
+#定义holiday_name变量记录节日
+#holiday_name = "生日"
+#if holiday_name == "情人节":
+#    print("买玫瑰")
+#    print("看电影")
+#elif holiday_name == "平安夜":
+#    print("买苹果")
+#    print("吃大餐")
+#elif holiday_name == "生日":
+#    print("买蛋糕")
+#    print("开房间")
+#else :
+#    print("每天都是节日")
